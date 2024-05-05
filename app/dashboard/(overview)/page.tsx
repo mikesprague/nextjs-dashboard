632deae7da91551230d0723e89c174d6d5bbc74e
Michael Sprague <mikesprague@gmail.com>
2024-05-05 11:13:53 -0400

    refactor(dashboard): move into (overview) route group
    also adds loading file

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,13 @@
+import { Suspense } from 'react';
+
+import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
