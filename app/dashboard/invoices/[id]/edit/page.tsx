a78cba96b4fabf85020169188dd5662e05b9bbe1
Michael Sprague <mikesprague@gmail.com>
2024-05-05 20:16:01 -0400

    feat(chapter-16): add metadata to pages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-import Form from '@/app/ui/invoices/edit-form';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import Form from '@/app/ui/invoices/edit-form';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
